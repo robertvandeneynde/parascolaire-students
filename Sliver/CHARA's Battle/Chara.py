c9aed0ef4eeca2c65b7482e626ee0a1166cc5866
Robert Vanden Eynde <robertvandeneynde@hotmail.com>
2018-01-23 16:44:46 +0100

    Jan 11 Jeudi

---
+++ Sliver/CHARA's Battle/Chara.py
@@ -0,0 +1,212 @@
+import random
+import pygame
+pygame.init()
+taille = [700, 500]
+ecran = pygame.display.set_mode(taille)
+
+
+NOIR = [0, 0, 0]
+BLANC = [255, 255, 255]
+GRIS = [99, 99, 99]
+VERT = [0, 255, 0]
+JAUNE = [255, 255, 0]
+ROUGE = [255, 0, 0]
+
+font = pygame.font.SysFont('Impact', 20)
+
+def deroulementDuCombat():
+    fini = 0
+    while(fini):
+        pygame
+
+        #Afficher le coeur et g??rer son comportement
+joueurEstMort = 0
+
+
+X = 30
+Y = 450
+X2 = X + 16
+Y2 = Y + 16
+Zone_X = 185
+Zone_Y = 180
+Combat = 1
+PV = 99
+PVCHARA = 500
+Action = 0
+attaque = 0
+enemi_X1 = 200
+enemi_Y1 = 200
+enemi_X2 = enemi_X1 + 83
+enemi_Y2 = enemi_Y1 + 22
+
+Coeur = pygame.image.load('Heart.png').convert_alpha()
+Zone = pygame.image.load('Zone.png').convert_alpha()
+Zone2 = pygame.image.load('Zone2.png').convert_alpha()
+COMBAT = pygame.image.load('COMBAT.png').convert_alpha()
+ACTION = pygame.image.load('ACTION.png').convert_alpha()
+OBJET = pygame.image.load('OBJET.png').convert_alpha()
+CLEMENCE = pygame.image.load('CLEMENCE.png').convert_alpha()
+Info = pygame.image.load('PlayerInfo.png').convert_alpha()
+ACTIONInfo = pygame.image.load('ACTIONINFO.png').convert_alpha()
+Knife = pygame.image.load('Knife.png').convert_alpha()
+GAME_OVER = pygame.image.load('GAME_OVER.png').convert_alpha()
+
+musique = pygame.mixer.Sound('REALMEGALOVANIA.ogg')
+damage = pygame.mixer.Sound('TakeDamage.ogg')
+attack = pygame.mixer.Sound('attack.wav')
+Determination = pygame.mixer.Sound('Determination.ogg')
+musique.play(loops=-1, maxtime=0, fade_ms=0)
+
+clock = pygame.time.Clock()
+
+class elementMenu:
+    def __init__(self, nomImage, positionX, positionY, ecran):
+        self.Image = pygame.image.load(nomImage).convert_alpha()
+        self.posX = positionX
+        self.posY = positionY
+        self.ecran = ecran
+    def dessiner(self):
+        self.ecran.blit(self.Image, [self.posX, self.posY])
+
+listeElementMenu = []
+
+listeElementMenu.append(elementMenu('CharaCombat.png', 280, 50, ecran))
+
+fini = 0
+while fini == 0:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            fini = 1
+
+        elif event.type == pygame.KEYDOWN:
+            if Combat == 1:
+                if event.key == 276:
+                    X = X - 160
+                if event.key == 275:
+                    X = X + 160
+                if event.key == 13:
+                    if X == 30:
+                        if Y == 450:
+                            deroulementDuCombat()
+    
+                            PVCHARA = PVCHARA -25
+                            Combat = 0
+                            X = 334
+                            Y = 275
+                            attack.play()
+                    if X == 190:
+                        if Y == 450:
+                            Zone = ACTIONInfo
+                            Zone_X = 85
+                            X = 105
+                            Y = 215
+                            Combat = 2
+            elif Combat == 2:
+                if event.key == 276:
+                    X = X - 265
+                if event.key == 275:
+                    X = X + 265
+                if event.key == 303:
+                    if Zone == ACTIONInfo:
+                        Combat == 1
+                        Zone == Zone
+                        X == 30
+                        Y == 450
+                        
+                if X < 105:
+                    X = 105
+                if X > 370:
+                    X = 370
+
+
+
+    pressed = pygame.key.get_pressed()
+
+    if pressed[276]:
+        if Combat == 0:
+            X = X - 3
+
+    if pressed[275]:
+        if Combat == 0:
+            X = X + 3
+
+    if pressed[273]:
+        if Combat == 0:
+            Y = Y - 3
+
+    if pressed[274]:
+        if Combat == 0:
+            Y = Y + 3
+
+
+
+    if Combat ==1:
+        if X < 30:
+            X = 30
+        if X > 510:
+            X = 510
+
+    if Combat == 0:
+        if X < 189:
+            X = 189
+        if X > 465:
+            X = 465
+        if Y < 184:
+            Y = 184
+        if Y > 360:
+            Y = 360
+
+    if PV > 99:
+        PV = 99
+
+
+    if X < enemi_X1:  # perso est trop ?? gauche
+        PV == PV
+    elif X > enemi_X2:  # perso est trop ?? droite
+        PV == PV
+    elif Y < enemi_Y1:  # perso est trop en haut
+        PV == PV
+    elif Y > enemi_Y2:  # perso est trop en bas
+        PV == PV
+    else:
+        PV = PV - 1
+        damage.play()
+
+    ecran.fill(NOIR)
+
+    ecran.blit (Info, [22, 400])
+    ecran.blit (COMBAT, [22, 435])
+    ecran.blit (ACTION, [182, 435])
+    ecran.blit (OBJET, [342, 435])
+    ecran.blit (CLEMENCE, [502, 435])
+    ecran.blit (Zone, [Zone_X, Zone_Y])
+    ecran.blit (Knife, [enemi_X1, enemi_Y1])
+    ecran.blit (Coeur, [X, Y])
+
+    for element in listeElementMenu:
+        element.dessiner()
+
+    pygame.draw.rect(ecran, ROUGE, [255,400, 99,16])
+    pygame.draw.rect(ecran, JAUNE, [255,400, PV,16])
+
+    image_PV = font.render(str(PV) + "/99", True, BLANC)
+    ecran.blit(image_PV, [370,395])
+
+    pygame.draw.rect(ecran, GRIS, [100,20, 500,16])
+    pygame.draw.rect(ecran, VERT, [100,20, PVCHARA,16])
+
+    #GAME OVER
+
+    if PV < 0 and joueurEstMort == 0:
+        musique.stop()
+        Determination.play(loops=-1, maxtime=0, fade_ms=0)
+        joueurEstMort = 1
+
+    if joueurEstMort == 1:
+        ecran.blit(GAME_OVER, [0, 0])
+    pygame.display.flip()
+
+    clock.tick(60)
+
+pygame.quit()
---
+++ Sliver/gl_3d_basics/gl_matrices_and_interpolation.py
@@ -0,0 +1,188 @@
+#!coding: utf-8
+from __future__ import print_function, division
+
+from OpenGL.GL import *
+from OpenGL.GL import shaders
+import ctypes
+import pygame
+from math import sin, cos, degrees, radians, tan
+
+import numpy
+from numpy import array, linalg
+
+from vec3_utils import * # téléchargez vec3_utils [ici](https://robertvandeneynde.be/parascolaire/vec3_utils.py.html)
+
+vertex_shader = """
+#version 330
+in vec3 position;
+in vec3 color;
+
+uniform mat4 pvmMatrix;
+
+out vec3 fcolor;
+
+void main()
+{
+    gl_Position = pvmMatrix * vec4(position, 1);
+    fcolor = color;
+}
+"""
+
+fragment_shader = """
+#version 330
+
+in vec3 fcolor;
+out vec4 pixel;
+
+void main()
+{
+    pixel = vec4(fcolor, 1);
+}
+"""
+
+vertices = farray([
+     0, 0, 0.60,
+     1, 0, 0,
+     0, 1, 0,
+     
+     1, 0, 0,
+     0, 1, 0,
+     1, 1, 1,
+     
+     0, 0, 0.60,
+     -1, 0, 0,
+     0, 1, 0,
+     
+     -1, 0, 0,
+     0, 1, 0,
+     -1, 1, 1,
+])
+
+colors = farray([
+     1, 0, 0,
+     0, 1, 0,
+     0, 0, 1,
+     
+     1, 0, 0,
+     0, 1, 0,
+     0, 0, 1,
+     
+     1, 0, 0,
+     0, 1, 0,
+     0, 0, 1,
+     
+     1, 0, 0,
+     0, 1, 0,
+     0, 0, 1,
+])
+
+def create_object(shader):
+    # Create a new VAO (Vertex Array Object) and bind it
+    vertex_array_object = glGenVertexArrays(1)
+    glBindVertexArray( vertex_array_object )
+    
+    # Get the position of the 'position' in parameter of our shader and bind it.
+    position = glGetAttribLocation(shader, 'position')
+    if position != -1: # maybe the attribute is useless and was discarded by the compiler
+        glEnableVertexAttribArray(position)
+    
+        # Generate buffers to hold our vertices
+        vertex_buffer = glGenBuffers(1)
+        glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer)
+        
+        # Describe the position data layout in the buffer
+        glVertexAttribPointer(position, 3, GL_FLOAT, False, 0, ctypes.c_void_p(0))
+    
+        # Send the data over to the buffer
+        glBufferData(GL_ARRAY_BUFFER, ArrayDatatype.arrayByteCount(vertices), vertices, GL_STATIC_DRAW)
+    else:
+        print('Inactive attribute "{}"'.format('position'))
+    
+    # Get the position of the 'position' in parameter of our shader and bind it.
+    color = glGetAttribLocation(shader, 'color')
+    
+    if color != -1: # maybe the attribute is useless and was discarded by the compiler
+        glEnableVertexAttribArray(color)
+        
+        # Generate buffers to hold our colors
+        vertex_buffer_color = glGenBuffers(1)
+        glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_color)
+    
+        # Describe the color data layout in the buffer
+        glVertexAttribPointer(color, 3, GL_FLOAT, False, 0, ctypes.c_void_p(0))
+        
+        # Send the data over to the buffer
+        glBufferData(GL_ARRAY_BUFFER, ArrayDatatype.arrayByteCount(colors), colors, GL_STATIC_DRAW)
+    else:
+        print('Inactive attribute "{}"'.format('color'))
+    
+    # Unbind the VAO first
+    glBindVertexArray(0)
+    
+    # Unbind the VBO
+    glBindBuffer(GL_ARRAY_BUFFER, 0)
+    
+    return vertex_array_object
+    
+def display(shader, vertex_array_object, t):
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glUseProgram(shader)
+    
+    # build projection matrix
+    p = PerspectiveMatrix(45, 1.0 * 512/512, 0.1, 100)
+    
+    # build view matrix (lookAt)
+    v = LookAtMatrix(vec3(7 * polar(0.5 * t), 5), (0, 0, 0), (0, 0, 1))
+    pv = p @ v
+    
+    # build model matrix of triangles (bouncy animation)
+    m = TranslationMatrix(0, 0, abs(sin(3*t)))
+    pvm = pv @ m
+    
+    # use our only vao
+    glBindVertexArray(vertex_array_object)
+    
+    # set matrix
+    loc_matrix = glGetUniformLocation(shader, 'pvmMatrix')
+    glUniformMatrix4fv(loc_matrix, 1, True, pvm)
+    
+    # draw
+    glPointSize(5)
+    glDrawArrays(GL_TRIANGLES, 0, 18)
+    
+    # unbind
+    glBindVertexArray(0)
+    glUseProgram(0)
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((512, 512), pygame.OPENGL | pygame.DOUBLEBUF)
+    glClearColor(0.5, 0.5, 0.5, 1.0)
+    # glViewport(0, 0, 512, 512) # we don't have to do that because it's the size of the window
+    glEnable(GL_DEPTH_TEST)
+
+    shader = shaders.compileProgram(
+        shaders.compileShader(vertex_shader, GL_VERTEX_SHADER),
+        shaders.compileShader(fragment_shader, GL_FRAGMENT_SHADER))
+    
+    vertex_array_object = create_object(shader)
+    
+    clock = pygame.time.Clock()
+    
+    tick = 0
+    done = False
+    while not done:     
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+        
+        tick += 1
+        display(shader, vertex_array_object, tick/60)
+        pygame.display.flip()
+        clock.tick(60)
+
+if __name__ == '__main__':
+    try:
+        main()
+    finally:
+        pygame.quit()
