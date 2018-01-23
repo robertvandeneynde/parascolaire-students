ab88714c8de7c979121e8d1cc2a2e139fcf61ccd
Robert Vanden Eynde <robertvandeneynde@hotmail.com>
2018-01-23 16:42:43 +0100

    Nov 16 Jeudi

---
+++ marouane/marouane.13.py
@@ -0,0 +1,21 @@
+a = 2
+b = 6
+c = 1
+
+if c<a : 
+    if b> a : 
+        print(c,a,b)
+    else : 
+        if b>c :
+            print (c,b,a)
+            else :
+                ...
+                
+                
+
+# comme l'exercice précédent, mais avec 3 valeurs !
+# écris un programme qui affiche les 3 valeurs dans l'ordre croissant
+# ici le programme va donc afficher 1 2 6
+# mais si on avait a=1 b=6 c=2 on aurait aussi eu 1 2 6
+# autre exemple : a=1 b=1 c=0 : affiche 0 1 1
+# indice : on peut mettre des if dans des if !
